4956b739944be8d0c72449a34c8520abbdb69d0b
shafriki <shafriki.cse@gmail.com>
2025-04-22 18:16:57 +0600

    added about section in home page

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import BannerCompo from './Banner/BannerCompo';
 import HomeProduct from './HomeProduct/HomeProduct';
+import About from './About/About';
 
 const Home = () => {
     return (
         <div>
             <BannerCompo />
+            <About />
             <HomeProduct />
         </div>
     );
